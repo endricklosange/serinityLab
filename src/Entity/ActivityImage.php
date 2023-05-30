6b63c6a8464352a76c3bd892b4040aa4a578e1cd
endricklosange <endricklosange@gmail.com>
2023-05-30 18:29:09 +0200

    feat: Adding the activity creation form that contains several forms for creating service and image linked to the activity

---
+++ src/Entity/ActivityImage.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ActivityImageRepository;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+
+#[ORM\Entity(repositoryClass: ActivityImageRepository::class)]
+#[Vich\Uploadable]
+class ActivityImage
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+    
+    #[Vich\UploadableField(mapping: 'activity', fileNameProperty: 'name', size: 'size')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(length: 255)]
+
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $size = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'activityImages')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Activity $activity = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function setImageFile(?File $imageFile){
+        $this->imageFile = $imageFile;
+        if (null !== $imageFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+        return $this;
+    }
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->size;
+    }
+
+    public function setSize(int $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getActivity(): ?Activity
+    {
+        return $this->activity;
+    }
+
+    public function setActivity(?Activity $activity): self
+    {
+        $this->activity = $activity;
+
+        return $this;
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ Encore
 
         // only copy files matching this pattern
         // pattern: /\.(png|jpg|jpeg)$/
+        
     })
     /*
      * ENTRY CONFIG
